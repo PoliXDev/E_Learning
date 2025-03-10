fdc5e88b43311149a72686c150bd2fcb50705205
DANIEL ALEJANDRO RUIZ POLI <danielruiz368@gmail.com>
2025-03-10 21:03:59 +0100

    project structure
    project structure and styles

---
+++ main.js
@@ -0,0 +1,8 @@
+// Importar estilos
+import './css/style.css';
+import './sass/app.scss';
+
+// Código JavaScript principal
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('E-Learning platform initialized');
+});
---
+++ webpack.config.prod.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './main.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true
+      }
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'img', to: 'img' },
+      ],
+    }),
+  ],
+  optimization: {
+    minimize: true,
+  },
+}; 
